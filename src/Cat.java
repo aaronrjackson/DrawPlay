36764314212ec63a1fb0dd6b00ef9444c9cc1588
Aaron Jackson <aaronjackson@mines.edu>
2024-09-24 16:48:30 -0600

    readded mouth, adjusted colors

--- src/Cat.java
+++ src/Cat.java
@@ -17,8 +17,8 @@ public class Cat {
 	private static final int MOUTH_HEIGHT = 4;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 9;
-	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 9;
+	private static final int MOUTH_X = HEAD_DIMENSION/5 * 3;
+	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 4;
 	
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
@@ -30,14 +30,14 @@ public class Cat {
 		g2.setColor(Color.orange);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
-		g2.setColor(Color.green);
+		g2.setColor(Color.blue);
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.pink);
+		g2.setColor(Color.red);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
